7472311085525d8e882585306ca981a673c5ef30
Jamen Marz <me@jamen.dev>
2019-12-25 09:48:18 -0700

    Fix DLL to be 32-bit lol.

--- crates/fable_cheat/src/lib.rs
+++ crates/fable_cheat/src/lib.rs
@@ -40,29 +40,29 @@ extern "system" fn DllMain(dll_handle: HINSTANCE, fdv_reason: DWORD, lpv_reserve
 }
 
 extern "system" fn init(lpThreadParameter: LPVOID) -> DWORD {
-    unsafe { AllocConsole() };
+    // unsafe { AllocConsole() };
 
     // Fable window search
-    let process_id = unsafe { GetCurrentProcessId() };
+    // let process_id = unsafe { GetCurrentProcessId() };
 
-    let mut fable_window_search = FableWindowSearch {
-        process_id: process_id,
-        hwnd: null_mut(),
-    };
+    // let mut fable_window_search = FableWindowSearch {
+    //     process_id: process_id,
+    //     hwnd: null_mut(),
+    // };
 
-    while fable_window_search.hwnd == null_mut() {
-        unsafe { EnumWindows(Some(find_fable_window), &mut fable_window_search as *mut FableWindowSearch as LPARAM) };
-    }
+    // while fable_window_search.hwnd == null_mut() {
+    //     unsafe { EnumWindows(Some(find_fable_window), &mut fable_window_search as *mut FableWindowSearch as LPARAM) };
+    // }
 
-    // error[E0133]: use of mutable static is unsafe and requires unsafe function or block
-    // note: mutable statics can be mutated by multiple threads: aliasing violations or data races will cause undefined
-    unsafe {
-        FABLE_WND_PROC = mem::transmute::<LONG_PTR, WNDPROC>(GetWindowLongPtrA(fable_window_search.hwnd, GWL_WNDPROC));
-    }
+    // // error[E0133]: use of mutable static is unsafe and requires unsafe function or block
+    // // note: mutable statics can be mutated by multiple threads: aliasing violations or data races will cause undefined
+    // unsafe {
+    //     FABLE_WND_PROC = Some(*(&GetWindowLongPtrA(fable_window_search.hwnd, GWL_WNDPROC) as *const _ as *const WNDPROC));
+    // }
 
-    unsafe {
-        SetWindowLongPtrA(fable_window_search.hwnd, GWL_WNDPROC, mem::transmute::<WNDPROC, LONG_PTR>(Some(wnd_proc_hook)));
-    }
+    // unsafe {
+    //     SetWindowLongPtrA(fable_window_search.hwnd, GWL_WNDPROC, mem::transmute::<WNDPROC, LONG_PTR>(Some(wnd_proc_hook) as i32));
+    // }
 
     // unsafe { ExitProcess(0) };
 
@@ -71,28 +71,28 @@ extern "system" fn init(lpThreadParameter: LPVOID) -> DWORD {
 
 // Fable window search callbacks
 
-extern "system" fn find_fable_window(hwnd: HWND, search: LPARAM) -> BOOL {
-    let mut search = unsafe { &mut *(search as *mut FableWindowSearch) };
+// extern "system" fn find_fable_window(hwnd: HWND, search: LPARAM) -> BOOL {
+//     let mut search = unsafe { &mut *(search as *mut FableWindowSearch) };
 
-    let mut process_id = 1 as DWORD;
+//     let mut process_id = 1 as DWORD;
 
-    unsafe { GetWindowThreadProcessId(hwnd, &mut process_id as LPDWORD) };
+//     unsafe { GetWindowThreadProcessId(hwnd, &mut process_id as LPDWORD) };
 
-    if process_id == search.process_id {
-        search.hwnd = hwnd;
-        0 as BOOL
-    } else {
-        1 as BOOL
-    }
-}
+//     if process_id == search.process_id {
+//         search.hwnd = hwnd;
+//         0 as BOOL
+//     } else {
+//         1 as BOOL
+//     }
+// }
 
-unsafe extern "system" fn wnd_proc_hook(hwnd: HWND, msg: u32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
-    match FABLE_WND_PROC {
-        Some(wnd_proc) => {
-            wnd_proc(hwnd, msg, wparam, lparam)
-        }
-        None => {
-            DefWindowProcA(hwnd, msg, wparam, lparam)
-        }
-    }
-}
+// unsafe extern "system" fn wnd_proc_hook(hwnd: HWND, msg: u32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
+//     match FABLE_WND_PROC {
+//         Some(wnd_proc) => {
+//             wnd_proc(hwnd, msg, wparam, lparam)
+//         }
+//         None => {
+//             DefWindowProcA(hwnd, msg, wparam, lparam)
+//         }
+//     }
+// }
