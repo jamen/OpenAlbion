8fff8b58306bb4aff983bfe0cb67e8049521028e
Jamen Marz <me@jamen.dev>
2019-12-26 02:06:23 -0700

    Make layout for launcher.

--- crates/defable/app/app.svelte
+++ crates/defable/app/app.svelte
@@ -1,50 +1,110 @@
 <script>
     import { remote } from 'electron'
     import addon from './index.node'
+    import path from 'path'
 
     let errors = []
     let mods = []
     let fableExecutable = ''
+    let defaultCheat = true
+    let defaultCheatDll = path.resolve(__dirname, './fable_cheat.dll')
+    let cheatDll = defaultCheatDll
 
     function launch () {
-        addon.launch_fable(fableExecutable)
+        addon.launch_fable(fableExecutable, cheatDll)
+    }
+
+    function selectFableExecutable () {
+        fableExecutable = openDialog()
+    }
+
+    function selectCheatDll () {
+        cheatDll = openDialog()
     }
 
     function openDialog () {
         let result = remote.dialog.showOpenDialog({ properties: [ 'openFile' ] })
-        if (result) result = result[0]
-        fableExecutable = result
+        return result && result[0]
     }
 
     function dismissErrors () {
         errors = []
     }
+
+    function addMod () {
+
+    }
+
+    function removeMod () {
+
+    }
+
+    function edit () {
+
+    }
 </script>
 
 <main>
     {#if errors.length > 0}
-        <div class="errors">
-            <img src="static/dismiss.svg" alt="Dismiss" on:click={dismissErrors}>
+        <section class="errors">
+            <img src="priority_high-24px.svg" alt="Oops.">
             <ul>
                 {#each errors as error}
                     <li>{error}</li>
                 {/each}
             </ul>
-        </div>
+            <img src="static/close-24px.svg" alt="Dismiss" on:click={dismissErrors}>
+        </section>
     {/if}
-    <div class="options">
-        <label>Fable executable<label>
-        <div class="file-select">
-            <input bind:value={fableExecutable} />
-            <button on:click={openDialog}>Select File</button>
-        </div>
-    </div>
-    <div class="mods">
-        {#each mods as mod}
-            <div class='mod'>
-
+    <section class="btns">
+        <button class='btn launch' on:click={launch}>
+            <img src="static/power_settings_new-24px.svg" alt="">
+            <span>Start</span>
+        </button>
+        <button class='btn edit' on:click={edit}>
+            <img src="static/tune-24px.svg" alt="">
+            <span>Edit</span>
+        </button>
+    </section>
+    <section class="options">
+        <h2>Options</h2>
+        <div class="list options-list">
+            <div class="option">
+                <h3>Fable.exe</h3>
+                <div class="file-select">
+                    <input type="text" bind:value={fableExecutable} />
+                    <button on:click={selectFableExecutable}>Select File</button>
+                </div>
             </div>
-        {/each}
-    </div>
-    <button on:click={launch}>Launch</button>
+            <div class="option">
+                <h3>DLL Cheat</h3>
+                <span>Use default cheat?</span>
+                <input type="checkbox" bind:checked={defaultCheat} >
+                <div class="file-select">
+                    <input type="text" bind:value={cheatDll} disabled={defaultCheat ? 'disabled' : ''} value={defaultCheat ? defaultCheatDll : cheatDll} />
+                    <button on:click={selectCheatDll} disabled={defaultCheat ? 'disabled' : ''}>Select File</button>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="mods">
+        <h2>Mods</h2>
+        <div class="list mods-list">
+            {#each mods as mod}
+                <div class='mod'>
+
+                </div>
+            {/each}
+        </div>
+    </section>
+    <section class="mods-options">
+        <button class="btn add" on:click={addMod}>
+            <img src="static/add-24px.svg" alt="">
+            <span>Add</span>
+        </button>
+        <button class="btn remove" on:click={removeMod}>
+            <img src="static/remove-24px.svg" alt="">
+            <span>Remove</span>
+        </button>
+    </section>
 </main>
--- crates/defable/native/src/lib.rs
+++ crates/defable/native/src/lib.rs
@@ -2,41 +2,56 @@
 extern crate neon;
 extern crate winapi;
 
+use std::ffi::OsStr;
 use std::ffi::CString;
 use std::ptr::null_mut;
 use std::mem;
 use std::path::Path;
+use std::os::windows::ffi::OsStrExt;
+use std::iter::once;
 
 use neon::prelude::*;
 
 use winapi::shared::minwindef::{DWORD,LPDWORD,LPVOID,FARPROC};
+use winapi::shared::basetsd::SIZE_T;
 
+use winapi::um::winbase::INFINITE;
 use winapi::um::minwinbase::LPTHREAD_START_ROUTINE;
 
 use winapi::um::winnt::{LPCSTR,LPSTR};
 use winapi::um::winnt::{MEM_RESERVE,MEM_COMMIT,PAGE_EXECUTE_READWRITE};
 use winapi::um::winnt::PROCESS_ALL_ACCESS;
+use winapi::um::winnt::{PROCESS_QUERY_INFORMATION,PROCESS_CREATE_THREAD,PROCESS_VM_OPERATION,PROCESS_VM_WRITE};
 
 use winapi::um::winbase::CREATE_SUSPENDED;
 
-use winapi::um::processthreadsapi::{CreateProcessA,ResumeThread,CreateRemoteThread,OpenProcess};
+use winapi::um::processthreadsapi::{CreateProcessA,ResumeThread,CreateRemoteThread,OpenProcess,GetExitCodeThread};
 use winapi::um::processthreadsapi::{STARTUPINFOA,PROCESS_INFORMATION};
 
 use winapi::um::memoryapi::{VirtualAllocEx,WriteProcessMemory};
+
 use winapi::um::libloaderapi::{GetProcAddress,GetModuleHandleA};
 
+use winapi::um::synchapi::WaitForSingleObject;
+
+use winapi::um::errhandlingapi::GetLastError;
+
 fn launch_fable(mut cx: FunctionContext) -> JsResult<JsUndefined> {
-    let fable_executable_original = cx.argument::<JsString>(0)?.value();
-    let fable_executable = fable_executable_original.as_ptr() as LPSTR;
+    // Arguments
+    let fable_executable_str = cx.argument::<JsString>(0)?.value();
+    let fable_executable = fable_executable_str.as_ptr() as LPSTR;
 
-    let dll_path_original = CString::new("C:\\Users\\Jamen\\Repos\\defable\\target\\i686-pc-windows-msvc\\debug\\fable_cheat.dll").unwrap();
-    let dll_path = dll_path_original.as_ptr() as LPCSTR;
+    let dll_path_str = cx.argument::<JsString>(1)?.value();
+    let dll_path = CString::new(dll_path_str).unwrap();
+    let dll_path_size = dll_path.to_bytes_with_nul().len();
 
+    // Returned values
     let mut process_info: PROCESS_INFORMATION = Default::default();
     let mut startup_info: STARTUPINFOA = Default::default();
 
     startup_info.cb = mem::size_of::<STARTUPINFOA>() as u32;
 
+    // Create Fable process
     if unsafe {
         CreateProcessA(
             null_mut(),
@@ -54,49 +69,84 @@ fn launch_fable(mut cx: FunctionContext) -> JsResult<JsUndefined> {
         panic!("Failed to execute the specified file.");
     }
 
-    if unsafe { ResumeThread(process_info.hThread) } == 0 {
-        panic!("Failed to resume the main thread.")
-    };
-
-    // let hprocess = unsafe { OpenProcess(PROCESS_ALL_ACCESS, 0, process_info.dwProcessId) };
     let hprocess = process_info.hProcess;
 
-    // Put DLL path in Fable's memory.
-
+    // Allocate area for DLL in process's memory.
     let dll_path_in_remote = unsafe {
-        VirtualAllocEx(hprocess, null_mut(), dll_path_original.to_bytes().len(), MEM_RESERVE | MEM_COMMIT, PAGE_EXECUTE_READWRITE)
+        VirtualAllocEx(
+            hprocess,
+            null_mut(),
+            dll_path_size,
+            MEM_RESERVE | MEM_COMMIT,
+            PAGE_EXECUTE_READWRITE
+        )
     };
 
-    println!("dll_path_in_remote {:?} {:?}", dll_path_in_remote, dll_path_original.to_bytes().len());
+    if dll_path_in_remote.is_null() {
+        panic!("Failed to allocate memory in remote process.");
+    }
 
-    unsafe {
-        WriteProcessMemory(hprocess, dll_path_in_remote, dll_path as LPVOID, dll_path_original.to_bytes().len(), null_mut())
+    // Write DLL to process's memory.
+    if unsafe {
+        WriteProcessMemory(
+            hprocess,
+            dll_path_in_remote,
+            dll_path.as_ptr() as LPVOID,
+            dll_path_size,
+            null_mut(),
+        )
+    } == 0 {
+        panic!("Failed to write DLL to remote process's memory.");
     };
 
-    println!("wrote memory");
+    println!("Wrote");
 
-    let load_library_in_remote = unsafe {
+    // Get LoadLibrary's address for any process.
+    let load_library_address = unsafe {
         GetProcAddress(
             GetModuleHandleA(CString::new("kernel32.dll").unwrap().as_ptr() as LPCSTR),
             CString::new("LoadLibraryA").unwrap().as_ptr() as LPCSTR
         )
     };
 
-    println!("load_library_in_remote {:?}", load_library_in_remote);
+    if load_library_address.is_null() {
+        panic!("Could not find LoadLibraryA in kernel32.dll");
+    }
 
+    println!("LoadLibraryA {:?}", load_library_address);
+
+    // Call LoadLibrary in remote process.
     let remote_thread = unsafe {
         CreateRemoteThread(
             hprocess,
             null_mut(),
             0,
-            Some(mem::transmute::<FARPROC, unsafe extern "system" fn(LPVOID) -> DWORD>(load_library_in_remote)),
+            Some(mem::transmute::<FARPROC, unsafe extern "system" fn(lpThreadParameter: LPVOID) -> DWORD>(load_library_address)),
             dll_path_in_remote as LPVOID,
             0,
             null_mut()
         )
     };
 
-    println!("remote_thread {:?}", remote_thread);
+    unsafe { WaitForSingleObject(remote_thread, INFINITE) };
+
+    let mut exit_code: DWORD = 0;
+
+    unsafe { GetExitCodeThread(remote_thread, &mut exit_code); }
+
+    if exit_code == 0 {
+        println!("Failed with {}", unsafe { GetLastError() });
+        panic!("Failed to CreateRemoteThread");
+    }
+
+    println!("Injected (exit code {})", exit_code);
+
+    // Resume Fable
+    if unsafe { ResumeThread(process_info.hThread) } == 0 {
+        panic!("Failed to resume the main thread.")
+    };
+
+    println!("Resumed");
 
     Ok(JsUndefined::new())
 }
