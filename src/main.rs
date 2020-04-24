5746af3a8abfc0442c65c88b33ebff8442042a3b
Jamen Marz <me@jamen.dev>
2020-04-24 14:05:21 -0600

    Update with new ember API.

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,53 @@ use std::fs::File;
 use std::path::PathBuf;
 use std::io::Read;
 
-use ember::{Ember,EmberConfig};
-use ember::winit::event::{Event,WindowEvent,KeyboardInput,VirtualKeyCode,ElementState};
-use ember::winit::event_loop::ControlFlow;
+use ember::{App,Context,Config};
+// use ember::winit::event::{Event,WindowEvent,KeyboardInput,VirtualKeyCode,ElementState};
+// use ember::winit::event_loop::ControlFlow;
 
 use fable_data::{Wad,Big,Decode,Entry};
 // use fable_data::Lev;
 
-pub struct Game {
+pub struct OpenAlbion {
     pub fable_path: PathBuf,
+}
+
+impl App for OpenAlbion {
+    fn init(&self, context: Context) {
+        // let mut wad_path = game.fable_path.clone();
+
+        // wad_path.push("Data/Levels/FinalAlbion.wad");
+
+        // let mut wad_file = File::open(wad_path).unwrap();
+
+        // let wad = Wad::decode(&mut wad_file);
+
+        // println!("wad {:#?}", wad);
+
+        let mut big_path = game.fable_path.clone();
+
+        big_path.push("Data/graphics/pc/frontend.big");
+
+        let mut big_file = File::open(big_path).unwrap();
+
+        let mut big = Big::decode(&mut big_file).unwrap();
+
+        let mut big_entry = big.entries.entries.get_mut(0).unwrap();
+
+        println!("big entry {:#?}", big_entry);
+
+        let mut big_entry_reader = big_entry.reader(&mut big_file).unwrap();
+        let mut big_entry_data = Vec::new();
+
+        big_entry_reader.read_to_end(&mut big_entry_data);
+
+        // println!("big entry data {:?}", big_entry_data);
+
+        loop {
+            context.window_update_sender.send(ember::Message::Test);
+            std::thread::sleep_ms(1000);
+        }
+    }
 }
 
 fn main() {
@@ -23,81 +61,17 @@ fn main() {
         None => std::env::current_dir().unwrap(),
     };
 
-    let game = Game {
+    let game = OpenAlbion {
         fable_path: fable_path,
     };
 
-    // let mut wad_path = game.fable_path.clone();
-
-    // wad_path.push("Data/Levels/FinalAlbion.wad");
-
-    // let mut wad_file = File::open(wad_path).unwrap();
-
-    // let wad = Wad::decode(&mut wad_file);
-
-    // println!("wad {:#?}", wad);
-
-    let mut big_path = game.fable_path.clone();
-
-    big_path.push("Data/graphics/pc/frontend.big");
-
-    let mut big_file = File::open(big_path).unwrap();
-
-    let mut big = Big::decode(&mut big_file).unwrap();
-
-    let mut big_entry = big.entries.entries.get_mut(0).unwrap();
-
-    println!("big entry {:#?}", big_entry);
-
-    let mut big_entry_reader = big_entry.reader(&mut big_file).unwrap();
-    let mut big_entry_data = Vec::new();
-
-    big_entry_reader.read_to_end(&mut big_entry_data);
-
-    // println!("big entry data {:?}", big_entry_data);
-
-
-
-    let config = EmberConfig {
-        title: "Open Albion".to_string(),
-        width: 1024,
-        height: 768,
-        resources: std::env::current_dir().unwrap().join("out"),
-    };
-
-    let Ember {
-        winit_window,
-        winit_event_loop,
-        ..
-    } = Ember::new(config).expect("Failed to initialize app.");
-
-    winit_event_loop.run(move |event, _, control_flow| {
-        match event {
-            Event::WindowEvent { event, .. } => {
-                // println!("{:?}", event);
-                match event {
-                    WindowEvent::CloseRequested => {
-                        *control_flow = ControlFlow::Exit;
-                    }
-                    WindowEvent::KeyboardInput { input: KeyboardInput { virtual_keycode, state, .. }, .. } => {
-                        match (virtual_keycode, state) {
-                            (Some(VirtualKeyCode::Escape), ElementState::Pressed) => {
-                                *control_flow = ControlFlow::Exit;
-                            },
-                            _key => {}
-                        }
-                    },
-                    _window_event => {}
-                }
-            },
-            Event::MainEventsCleared => {
-                winit_window.request_redraw()
-            },
-            Event::RedrawRequested(_window_id) => {
-                // TODO: Draw
-            },
-            _event => {}
+    game.start(
+        Config {
+            title: "Open Albion".to_string(),
+            width: 1024,
+            height: 768,
+            resources: std::env::current_dir().unwrap().join("out"),
         }
-    })
+    )
 }
 
