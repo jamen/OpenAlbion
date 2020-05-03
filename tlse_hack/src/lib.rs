8c0315dae30ae829d8740c4daa11c8debe63f417
Jamen Marz <me@jamen.dev>
2020-05-02 22:39:06 -0600

    Add more data

--- tlse_hack/src/lib.rs
+++ tlse_hack/src/lib.rs
@@ -3,8 +3,11 @@
 
 pub mod loc;
 
+use loc::*;
+
 use std::ptr::null_mut;
 use std::io::{Write,BufRead};
+use std::convert::TryInto;
 
 // use winapi::ctypes::*;
 
@@ -36,28 +39,17 @@ unsafe extern "system" fn DllMain(dll_handle: HINSTANCE, fdv_reason: DWORD, lpv_
 unsafe extern "system" fn init(lpThreadParameter: LPVOID) -> DWORD {
     AllocConsole();
 
-    // let instr_ptr = 0x00a60093 as *mut [u8; 5];
-    // let instr_len = (*instr_ptr).len();
-    // let mut old_protection: u32 = 0;
+    // println!("maybe dev frontend {}", read(G_SHOW_DEV_FRONTEND, 1)[0]);
+    // write(G_SHOW_DEV_FRONTEND, &[ 1 ]);
+    // println!("maybe dev frontend {}", read(G_SHOW_DEV_FRONTEND, 1)[0]);
 
-    // println!("instr_ptr {:x?}", instr_ptr);
-    // println!("instr_ptr len {:?}", instr_len);
+    println!("fullscreen {}", read(G_FULL_SCREEN, 1)[0]);
+    write(G_FULL_SCREEN, &[ 0 ]);
+    println!("fullscreen {}", read(G_FULL_SCREEN, 1)[0]);
 
-    // VirtualProtectEx(GetCurrentProcess(), instr_ptr as LPVOID, instr_len, PAGE_EXECUTE_READWRITE, &mut old_protection);
-    // println!("instr {:x?}", *instr_ptr);
-
-    // std::ptr::write_bytes(instr_ptr, 0, instr_len);
-
-    // let style_bytes = WS_BORDER.to_le_bytes();
-    // println!("style_bytes {:x?}", style_bytes);
-    // (*instr_ptr)[1] = style_bytes[0];
-    // (*instr_ptr)[2] = style_bytes[1];
-    // (*instr_ptr)[3] = style_bytes[2];
-    // (*instr_ptr)[4] = style_bytes[3];
-
-    // VirtualProtectEx(GetCurrentProcess(), instr_ptr as LPVOID, instr_len, old_protection, null_mut());
-
-    // println!("modified instr {:x?}", *instr_ptr);
+    println!("width {}", u32::from_le_bytes(read(G_RESOLUTION_WIDTH, 4).try_into().unwrap()));
+    println!("height {}", u32::from_le_bytes(read(G_RESOLUTION_HEIGHT, 4).try_into().unwrap()));
+    println!("refresh rate {}", u32::from_le_bytes(read(G_RESOLUTION_REFRESH_RATE, 4).try_into().unwrap()));
 
     println!("flush instruction cache {}", FlushInstructionCache(GetCurrentProcess(), null_mut(), 0));
 
@@ -82,46 +74,40 @@ unsafe fn run_prompt() {
         match line.as_ref() {
             // ...
             "" => println!("No command given."),
-            "toggle_debug_profile" => {
-                let debug_profile_ptr = (0x00400000 + 0xF75741) as *mut u8;
-                let mut old_protection: u32 = 0;
-
-                VirtualProtectEx(GetCurrentProcess(), debug_profile_ptr as LPVOID, 3, PAGE_EXECUTE_READWRITE, &mut old_protection);
-
-                println!("debug_profile data {}", *debug_profile_ptr);
-
-                if (*debug_profile_ptr) == 0 {
-                    (*debug_profile_ptr) = 1;
+            "dbg_profile" => {
+                if read(G_ALLOW_DEBUG_PROFILE, 1)[0] == 0 {
+                    write(G_ALLOW_DEBUG_PROFILE, &[ 1 ]);
+                    write(C_USER_PROFILE_MANAGER__IS_DEBUG_PROFILE, &[ 0xb0, 0x01 ]);
+                    println!("enabled");
                 } else {
-                    (*debug_profile_ptr) = 0;
+                    write(G_ALLOW_DEBUG_PROFILE, &[ 0 ]);
+                    write(C_USER_PROFILE_MANAGER__IS_DEBUG_PROFILE, &[ 0x32, 0xc0 ]);
+                    println!("disabled");
                 }
-
-                println!("debug_profile data {}", *debug_profile_ptr);
-
-                VirtualProtectEx(GetCurrentProcess(), debug_profile_ptr as LPVOID, 3, old_protection, null_mut());
-
-                let debug_profile_ptr = 0x407030 as *mut [u8; 3];
-                let mut old_protection: u32 = 0;
-
-                VirtualProtectEx(GetCurrentProcess(), debug_profile_ptr as LPVOID, 3, PAGE_EXECUTE_READWRITE, &mut old_protection);
-
-                // println!("old_protection {:x?}", old_protection);
-
-                println!("debug_profile fn {:x?}", *debug_profile_ptr);
-
-                if (*debug_profile_ptr)[0] == 0x32 {
-                    (*debug_profile_ptr)[0] = 0xb0;
-                    (*debug_profile_ptr)[1] = 0x01;
-                } else {
-                    (*debug_profile_ptr)[0] = 0x32;
-                    (*debug_profile_ptr)[1] = 0xc0;
-                }
-
-                VirtualProtectEx(GetCurrentProcess(), debug_profile_ptr as LPVOID, 3, old_protection, null_mut());
-
-                println!("debug_profile fn {:x?}", *debug_profile_ptr);
-            }
+            },
             _ => println!("Unknown command."),
         }
     }
 }
+
+unsafe fn read<'a>(address: usize, length: usize) -> &'a [u8] {
+    std::slice::from_raw_parts(address as *mut u8, length)
+}
+
+unsafe fn write(address: usize, buffer: &[u8]) {
+    let len = buffer.len();
+
+    let mut protect: u32 = 0;
+    VirtualProtectEx(GetCurrentProcess(), address as LPVOID, len, PAGE_EXECUTE_READWRITE, &mut protect);
+
+    std::ptr::copy(buffer.as_ptr(), address as *mut u8, len);
+
+    VirtualProtectEx(GetCurrentProcess(), address as LPVOID, len, protect, null_mut());
+}
+
+// unsafe fn write_restore(address: usize, buffer: &[u8]) -> impl Fn() {
+//     let mut restore: Vec<u8> = Vec::with_capacity(buffer.len());
+//     restore.copy_from_slice(buffer);
+//     write(address, buffer);
+//     move || write(address, &restore)
+// }
--- tlse_hack/src/loc.rs
+++ tlse_hack/src/loc.rs
@@ -6,55 +6,91 @@
 
 #![allow(dead_code)]
 
-pub mod func {
-    pub const ENTRY: u32 = 0x00401067;
-    pub const WIN_MAIN: u32 = 0x00403480;
-    /// The top-most init function.
-    pub const GF_MAIN: u32 = 0x00402510;
-    pub const GF_RUN_INIT_SCRIPTS: u32 = 0x00413c50;
-    pub const C_SYSTEM_MANAGER_INIT: u32 = 0x00403b10;
-    pub const CF_GET_SYSTEM_MANAGER: u32 = 0x009a4ec0;
-    pub const C_GAME__PLAY: u32 = 0x00412f90;
+//
+// Functions
+//
 
-    pub const C_MAIN_GAME_COMPONENT__INIT_GRAPHICS: u32 = 0x00416c8a;
-    pub const C_MAIN_GAME_COMPONENT__INITIALISE_FONTS: u32 = 0x00416c8a;
+pub const ENTRY: usize = 0x401067;
+pub const WIN_MAIN: usize = 0x403480;
+/// The top-most init function.
+pub const GF_MAIN: usize = 0x402510;
+pub const GF_RUN_INIT_SCRIPTS: usize = 0x413c50;
+pub const C_SYSTEM_MANAGER_INIT: usize = 0x403b10;
+pub const CF_GET_SYSTEM_MANAGER: usize = 0x9a4ec0;
+pub const C_GAME__PLAY: usize = 0x412f90;
 
-    pub const ZLIB_CRC32: u32 = 0x00c05fd0;
-    /// Very large function.
-    pub const C_CHAR_STRING__RUN_CUTSCENE_MACRO: u32 = 0x00cbfb7d;
+pub const C_MAIN_GAME_COMPONENT__INIT_GRAPHICS: usize = 0x416c8a;
+// pub const C_MAIN_GAME_COMPONENT__INITIALISE_FONTS: usize = 0x416c8a;
 
-    /// Good candidates for modding.
-    pub const C_SCRIPT_INFO_MANAGER__INIT: u32 = 0x00cb5d80;
-    pub const C_SCRIPT_INFO_MANAGER__REGISTER_SCRIPTS: u32 = 0x00cd52d0;
-    pub const C_SCRIPT_INFO_MANAGER__REGISTER_DEFS: u32 = 0x00f2a0f0;
+pub const ZLIB_CRC32: usize = 0xc05fd0;
+/// Very large function.
+pub const C_CHAR_STRING__CONSTRUCTOR: usize = 0x99ebf0;
+pub const C_CHAR_STRING__RUN_CUTSCENE_MACRO: usize = 0xcbfb7d;
+pub const C_CHAR_STRING__UNASSIGN_STRING: usize = 0x99e9b0;
+pub const C_CHAR_STRING__ALLOC_STRING_DATA: usize = 0x99ea60;
 
-    /// Parent of Direct3DCreate9 call.
-    pub const C_DISPLAY_MANAGER__INIT: u32 = 0x009c0e50;
-    /// Adjacent of Direct3DCreate9 call.
-    pub const C_DISPLAY_MANAGER__CREATE_DEVICE: u32 = 0x009bf7e0;
-    pub const C_RENDER_MANAGER_CORE__INIT: u32 = 0x00a0a940;
-    pub const C_SHADER_RENDER_MANAGER__INITIALISE: u32 = 0x0098acf0;
+pub const C_BASIC_STRING__SET_STRING: usize = 0x9a0300;
 
-    pub const C_WORLD__LOAD_GAME_STATE_INTERNAL: u32 = 0x004a21f0;
-    pub const C_WORLD__SAVE_GAME_STATE_INTERNAL: u32 = 0x0049f4c0;
-    pub const C_WORLD__ACTIVATE_WORLD: u32 = 0x0049f180;
+/// Good candidates for modding.
+pub const C_SCRIPT_INFO_MANAGER__INIT: usize = 0xcb5d80;
+pub const C_SCRIPT_INFO_MANAGER__REGISTER_SCRIPTS: usize = 0xcd52d0;
+pub const C_SCRIPT_INFO_MANAGER__REGISTER_DEFS: usize = 0xf2a0f0;
 
-    pub const N_SCRIPT__C_GAMEFLOW_SCRIPT__DECLARE_GOSSIP_CATEGORIES: u32 = 0x00ce6cf0;
-    /// Used in 0x00e6068b patch
-    pub const N_SCRIPT__CV_BODY_GAURD_SCRIPT__C_BODY_GAURD__MAIN: u32 = 0x00e5fd40;
+/// Parent of Direct3DCreate9 call.
+pub const C_DISPLAY_MANAGER__INIT: usize = 0x9c0e50;
+/// Adjacent of Direct3DCreate9 call.
+pub const C_DISPLAY_MANAGER__CREATE_DEVICE: usize = 0x9bf7e0;
+pub const C_RENDER_MANAGER_CORE__INIT: usize = 0xa0a940;
+pub const C_SHADER_RENDER_MANAGER__INITIALISE: usize = 0x98acf0;
 
-    pub const C_CONSOLE__INITIALISE: u32 = 0x009ed190;
+pub const C_WORLD__INIT: usize = 0x4a6e30;
+pub const C_WORLD__LOAD_GAME_STATE_INTERNAL: usize = 0x4a21f0;
+pub const C_WORLD__SAVE_GAME_STATE_INTERNAL: usize = 0x49f4c0;
+pub const C_WORLD__ACTIVATE_WORLD: usize = 0x49f180;
 
-    pub const C_USER_PROFILE_MANAGER__IS_DEBUG_PROFILE: u32 = 0x00407030;
-}
+pub const N_SCRIPT__C_GAMEFLOW_SCRIPT__DECLARE_GOSSIP_CATEGORIES: usize = 0xce6cf0;
+/// Used in 0xe6068b patch
+pub const N_SCRIPT__CV_BODY_GAURD_SCRIPT__C_BODY_GAURD__MAIN: usize = 0xe5fd40;
 
-pub mod data {
-    /// Referenced lots of items. Maybe an entry into a lot of the game state.
-    pub const P_MAIN_GAME_COMPONENT: u32 = 0x013b86a0;
-    pub const G_EDIT: u32 = 0x013b8605;
-    pub const G_DO_NOT_CALL_START_AUTO_SAVE_PROGRESS: u32 = 0x013b89d1;
-    pub const G_DO_NOT_CALL_STOP_AUTO_SAVE_PROGRESS: u32 = 0x013b89d0;
-    pub const MAYBE_HERO_STATS_DISPLAY: u32 = 0x005ce0e6;
-    /// Maybe the frame count?
-    pub const C_WORLD__FRAME: u32 = 0x013b89bc;
-}
+pub const C_CONSOLE__INITIALISE: usize = 0x9ed190;
+
+pub const C_USER_PROFILE_MANAGER__IS_DEBUG_PROFILE: usize = 0x407030;
+
+pub const C_NEW_FRONTEND_GAME_COMPONENT__COMPILE_DEFS: usize = 0x42f70b;
+pub const C_NEW_FRONTEND_GAME_COMPONENT__INITIALISE_DEFS: usize = 0x42f722;
+
+pub const N_GLOBAL_CONSOLE__INITIALISE: usize = 0x419d90;
+
+pub const C_PLAYER_GUI__ADD_SCREEN_MESSAGE: usize = 0x44bb90;
+
+
+//
+// Data
+//
+
+/// Referenced lots of items. Maybe an entry into a lot of the game state.
+pub const P_MAIN_GAME_COMPONENT: usize = 0x13b86a0;
+pub const P_GUI_DEF: usize = 0x13b878c;
+
+pub const G_EDIT: usize = 0x13b8605;
+pub const G_DO_NOT_CALL_START_AUTO_SAVE_PROGRESS: usize = 0x13b89d1;
+pub const G_DO_NOT_CALL_STOP_AUTO_SAVE_PROGRESS: usize = 0x13b89d0;
+pub const MAYBE_HERO_STATS_DISPLAY: usize = 0x5ce0e6;
+/// Maybe the frame count?
+pub const C_WORLD__FRAME: usize = 0x13b89bc;
+pub const G_ALLOW_DEBUG_PROFILE: usize = 0x1375741;
+pub const G_SHOW_DEV_FRONTEND: usize = 0x13b8642;
+
+pub const NO_STRINGS: usize = 0x13bd800;
+
+pub const G_FULL_SCREEN: usize = 0x137544a;
+pub const G_ANTIALIASING_ON: usize = 0x13b8607;
+pub const G_ANTIALIASING_X9: usize = 0x13b8608;
+pub const G_KICKOFF_SIZE: usize = 0x1375478;
+pub const G_PUSH_BUFFER_SIZE: usize = 0x1375474;
+pub const G_RESOLUTION_REFRESH_RATE: usize = 0x13b8628;
+pub const G_Z_DEPTH_BUFFER: usize = 0x13b8624;
+pub const G_RESOLUTION_WIDTH: usize = 0x1375468;
+pub const G_RESOLUTION_DEPTH: usize = 0x1375460;
+pub const G_PRESENT_IMMEDIATE: usize = 0x137545c;
+pub const G_RESOLUTION_HEIGHT: usize = 0x1375464;
