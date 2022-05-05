c65f86ab516b61c2e9dded86fb5ae583dd334572
Jamen Marz <me@jamen.dev>
2022-05-05 01:59:38 -0600

    save progress

--- fool/src/main.rs
+++ fool/src/main.rs
@@ -1,3 +1,26 @@
+use std::{
+    fs::{self, File, OpenOptions},
+    io::BufReader,
+};
+
+use clap::Parser;
+
+use fable_data::{Big, BigKind};
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    file: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+
+    let args = Args::parse();
+
+    let file = fs::read(&args.file).expect("Failed to open file");
+
+    let big = Big::parse(&mut &file[..], BigKind::guess_from_path(&args.file)).unwrap();
+
+    println!("file: {:?}", big);
 }
