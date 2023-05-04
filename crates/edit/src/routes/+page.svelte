e3934631f3e129ad42597e13b4ced9d0b54e8e7d
Jamen Marz <me@jamen.dev>
2023-05-04 15:11:33 -0400

    wip: reorganizing

---
+++ crates/edit/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { invoke } from "@tauri-apps/api/tauri";
+    import { onMount } from "svelte";
+
+    let message = "";
+
+    onMount(async () => {
+        message = await invoke("greet", { name: "world" });
+    });
+</script>
+
+{message}
