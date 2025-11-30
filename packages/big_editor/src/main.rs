449f837dc6a41fa40d94665c6fef535defef74fb
Jamen Marz <jamenmarz@gmail.com>
2025-11-30 12:02:27 -0500

    add GPUI example app

--- packages/big_editor/src/main.rs
+++ packages/big_editor/src/main.rs
@@ -1,3 +1,35 @@
+use gpui::*;
+use gpui_component::{button::*, *};
+
+pub struct HelloWorld;
+
+impl Render for HelloWorld {
+    fn render(&mut self, _: &mut Window, _: &mut Context<Self>) -> impl IntoElement {
+        div().h_flex().child(div().child("Hello")).child(
+            Button::new("ok")
+                .primary()
+                .label("Let's Go!")
+                .on_click(|_, _, _| println!("Clicked!")),
+        )
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let app = Application::new();
+
+    app.run(move |cx| {
+        // This must be called before using any GPUI Component features.
+        gpui_component::init(cx);
+
+        cx.spawn(async move |cx| {
+            cx.open_window(WindowOptions::default(), |window, cx| {
+                let view = cx.new(|_| HelloWorld);
+                // This first level on the window, should be a Root.
+                cx.new(|cx| Root::new(view, window, cx))
+            })?;
+
+            Ok::<_, anyhow::Error>(())
+        })
+        .detach();
+    });
 }
