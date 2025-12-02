05d0c6c5458f8f78eb9899bc6a19e6c697d5563b
Jamen Marz <jamenmarz@gmail.com>
2025-12-02 01:06:11 -0500

    impl counter in gpui

--- packages/big_editor/src/main.rs
+++ packages/big_editor/src/main.rs
@@ -1,29 +1,46 @@
-use gpui::*;
-use gpui_component::{button::*, *};
+use gpui::{
+    App, AppContext, Application, ClickEvent, Context, Entity, IntoElement, ParentElement, Render,
+    Window, WindowOptions, div,
+};
+use gpui_component::{
+    Root, StyledExt,
+    button::{Button, ButtonVariants},
+    table::Table,
+};
 
-pub struct HelloWorld;
+pub struct HelloWorld {
+    count: i64,
+}
+
+impl HelloWorld {
+    fn on_increment(&mut self, _: &ClickEvent, _window: &mut Window, _cx: &mut Context<Self>) {
+        self.count += 1;
+    }
+}
 
 impl Render for HelloWorld {
-    fn render(&mut self, _: &mut Window, _: &mut Context<Self>) -> impl IntoElement {
-        div().h_flex().child(div().child("Hello")).child(
-            Button::new("ok")
-                .primary()
-                .label("Let's Go!")
-                .on_click(|_, _, _| println!("Clicked!")),
-        )
+    fn render(&mut self, _: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
+        div()
+            .h_flex()
+            // .child(Table::new())
+            .child(format!("{}", &self.count))
+            .child(
+                Button::new("ok")
+                    .primary()
+                    .label("Let's Go!")
+                    .on_click(cx.listener(Self::on_increment)),
+            )
     }
 }
 
 fn main() {
-    let app = Application::new();
-
-    app.run(move |cx| {
+    Application::new().run(|cx: &mut App| {
         // This must be called before using any GPUI Component features.
         gpui_component::init(cx);
 
         cx.spawn(async move |cx| {
             cx.open_window(WindowOptions::default(), |window, cx| {
-                let view = cx.new(|_| HelloWorld);
+                let view = cx.new(|_| HelloWorld { count: 0 });
                 // This first level on the window, should be a Root.
                 cx.new(|cx| Root::new(view, window, cx))
             })?;
